b86d72a2424c4afdb94e577a958ba767c6a74220
Cekap Yakup <61888196+cekapykp@users.noreply.github.com>
2022-03-21 15:02:31 +0300

    v0.13
    Rehberdeki kişilerin bilgileri düzenlenebilir oldu. Düzenleme işlemleri local işlemdeki dizi için de geçerli.

--- js/app.js
+++ js/app.js
@@ -12,69 +12,75 @@ kisiListesi.addEventListener("click", kisiIslemleriniYap);
 
 //Tüm kişileri sonra localhosta kaydetebilmek için oluşturulacak dizi
 const tumKisilerDizisi = [];
+let secilenSatir = undefined;
+let sitedeYapilanEdit = 0; //Sitedeki editlerin sayısını tutacak
 
-function kaydet(e){
+function kaydet(e) {
     e.preventDefault();
-    const eklenecekKisi = {
+    const eklenecekVeyaGuncellenecekKisi = {
         ad: ad.value,
-        soyad : soyad.value,
-        mail:mail.value,
+        soyad: soyad.value,
+        mail: mail.value,
     }
-    const sonuc = verileriKontrolEt(eklenecekKisi);
-    if(sonuc.durum){
-        kisiyiEkle(eklenecekKisi);
-    }else{
-        bilgiOlustur(sonuc.mesaj,sonuc.durum)
+    const sonuc = verileriKontrolEt(eklenecekVeyaGuncellenecekKisi);
+    if (sonuc.durum) {
+        if (secilenSatir) { //Satir varsa güncelleme yap
+            kisiyiGuncelle(eklenecekVeyaGuncellenecekKisi);
+        } else {  //Kişiye ekle
+            kisiyiEkle(eklenecekVeyaGuncellenecekKisi);
+        }
+    } else {
+        bilgiOlustur(sonuc.mesaj, sonuc.durum)
     }
 }
 
-function verileriKontrolEt(kisi){
-    for(const deger in kisi){
-        if(kisi[deger]){
+function verileriKontrolEt(kisi) {
+    for (const deger in kisi) {
+        if (kisi[deger]) {
             console.log(kisi[deger]);
-        }else{
-            return{
-                durum : false,
+        } else {
+            return {
+                durum: false,
                 mesaj: "Boş alan bırakmayınız"
             }
         }
     }
     alanlarıTemizle();
-    return{
-        durum:true,
+    return {
+        durum: true,
         mesaj: `Kaydedildi`,
 
     }
 }
 
-function bilgiOlustur(mesaj,durum){
+function bilgiOlustur(mesaj, durum) {
     const olusturulanBilgi = document.createElement("div");
     olusturulanBilgi.textContent = mesaj;
 
     olusturulanBilgi.className = "bilgi";
 
     olusturulanBilgi.classList.add(durum ? "bilgi--success" : "bilgi--error")
-    document.querySelector(".container").insertBefore(olusturulanBilgi,form);
+    document.querySelector(".container").insertBefore(olusturulanBilgi, form);
 
-    setTimeout(function(){
+    setTimeout(function () {
         const silinecekDiv = document.querySelector(".bilgi");
-        if(silinecekDiv){
+        if (silinecekDiv) {
             silinecekDiv.remove();
         }
-    },2000);
+    }, 2000);
 }
 
-function alanlarıTemizle(){
+function alanlarıTemizle() {
     ad.value = "";
-    soyad.value="";
-    mail.value="";
+    soyad.value = "";
+    mail.value = "";
 }
 
-function kisiyiEkle(eklenecekKisi){
+function kisiyiEkle(eklenecekVeyaGuncellenecekKisi) {
     const olusturulanTrElementi = document.createElement("tr");
-    olusturulanTrElementi.innerHTML = `<td>${eklenecekKisi.ad}</td>
-    <td>${eklenecekKisi.soyad}</td>
-    <td>${eklenecekKisi.mail}</td>
+    olusturulanTrElementi.innerHTML = `<td>${eklenecekVeyaGuncellenecekKisi.ad}</td>
+    <td>${eklenecekVeyaGuncellenecekKisi.soyad}</td>
+    <td>${eklenecekVeyaGuncellenecekKisi.mail}</td>
     <td>
         <button  class="btn btn--edit">
             <i class="fa-solid fa-edit"></i>
@@ -84,22 +90,32 @@ function kisiyiEkle(eklenecekKisi){
         </button>
     </td>`;
     kisiListesi.appendChild(olusturulanTrElementi);
-    tumKisilerDizisi.push(eklenecekKisi);
-    bilgiOlustur("Kişi rehbere kaydedildi" ,true)
+    tumKisilerDizisi.push(eklenecekVeyaGuncellenecekKisi);
+    bilgiOlustur("Kişi rehbere kaydedildi", true)
 }
 
-function kisiIslemleriniYap(event){
+function kisiIslemleriniYap(event) {
 
-    if(event.target.classList.contains("btn--delete")){
+    if (event.target.classList.contains("btn--delete")) {
         const silinecekTR = event.target.parentElement.parentElement;
         const silinecekMail = event.target.parentElement.previousElementSibling.textContent;
-        rehberdenSil(silinecekTR,silinecekMail);
-    }else if(event.target.classList.contains("btn--edit")){
-        console.log("güncelleme")
+        rehberdenSil(silinecekTR, silinecekMail);
+    } else if (event.target.classList.contains("btn--edit")) {
+        document.querySelector(".kaydetGuncelle").value = "Guncelle";   //console.log("güncelleme")
+        const secilenTR = event.target.parentElement.parentElement;
+        //const guncellenecekMail = secilenTr.cells[2].textContent;
+
+        ad.value = secilenTR.cells[0].textContent;
+        soyad.value = secilenTR.cells[1].textContent;
+        mail.value = secilenTR.cells[2].textContent;
+
+        secilenSatir = secilenTR;
+
+
     }
 }
 
-function rehberdenSil(silinecekTrElement,silinecekMail){
+function rehberdenSil(silinecekTrElement, silinecekMail) {
     silinecekTrElement.remove();
 
     //Maile göre silme işlemi
@@ -108,9 +124,31 @@ function rehberdenSil(silinecekTrElement,silinecekMail){
     //         tumKisilerDizisi.splice(index,1)
     //     }
     // });
-    const silinmeyecekKisiler = tumKisilerDizisi.filter(function(kisi,index){
+    const silinmeyecekKisiler = tumKisilerDizisi.filter(function (kisi, index) {
         return kisi.mail !== silinecekMail
     });
     tumKisilerDizisi.length = 0;
     tumKisilerDizisi.push(...silinmeyecekKisiler);
+
+    alanlarıTemizle();
+    document.querySelector(".kaydetGuncelle").value="Submit"
+}
+
+function kisiyiGuncelle(kisi) {
+    //Seçilen satırda güncellenmemiş değerler var 
+    for (let i = 0; i < tumKisilerDizisi.length; i++) {
+        if (tumKisilerDizisi[i].mail === secilenSatir.cells[2].textContent) {
+            tumKisilerDizisi[i] = kisi;
+            break; 
+        }
+    }
+    //kişi paraametresinde seçilen kişinin yeni değerleri vardır.
+    secilenSatir.cells[0].textContent = kisi.ad;
+    secilenSatir.cells[1].textContent = kisi.soyad;
+    secilenSatir.cells[2].textContent = kisi.mail;
+
+    document.querySelector(".kaydetGuncelle").value = "Submit";
+    sitedeYapilanEdit++;
+    secilenSatir = undefined
+
 }
