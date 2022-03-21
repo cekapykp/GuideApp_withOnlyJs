116782c8462bf8b8e8eedba0a6a486efb31a75e4
Cekap Yakup <61888196+cekapykp@users.noreply.github.com>
2022-03-21 17:02:02 +0300

    v0.3
    Class yapısı ile localStorage ve arayüze kişi ekleme classları eklendi

--- js/class.js
+++ js/class.js
@@ -6,39 +6,96 @@ class Kisi {
     }
 }
 
+class Util { //Yararlı fonksiyonları içeren fonksiyon
+    static bosAlanKontrolEt(...alanlar) {
+        let sonuc = true;
+        alanlar.forEach(alan => {
+            if (alan.length === 0) {
+                sonuc = false;
+                return false;
+            }
+        });
+        return sonuc;
+    }
+}
+
 class Ekran {
     constructor() {
         this.ad = document.getElementById("ad");
         this.soyad = document.getElementById("soyad");
         this.mail = document.getElementById("mail");
-        this.ekleGuncelle = document.querySelector(".kaydetGuncelle");
-        
+        this.ekleGuncelleButton = document.querySelector(".kaydetGuncelle");
+        this.form = document.getElementById("form-rehber").addEventListener("submit", this.kaydetGuncelle.bind(this));
+
+        this.kisiListesi = document.querySelector(".kisi-listesi");
         this.depo = new Depo();
-        this.depo
+        this.kisilerEkranaYazdir();
+    }
+
+    kisilerEkranaYazdir() {
+        this.depo.tumKisiler.forEach(kisi => {
+            this.kisiyiEkranaEkle(kisi);
+        });
+    }
+
+    kisiyiEkranaEkle(kisi) {
+        const olusturlanTR = document.createElement("tr");
+        olusturlanTR.innerHTML = `<td>${kisi.ad}</td>
+        <td>${kisi.soyad}</td>
+        <td>${kisi.mail}</td>
+        <td>
+            <button  class="btn btn--edit">
+                <i class="fa-solid fa-edit"></i>
+            </button>
+            <button class="btn btn--delete">
+                <i class="fa-solid fa-trash"></i>
+            </button>
+        </td>`;
+        this.kisiListesi.appendChild(olusturlanTR);
+
+    }
+
+    kaydetGuncelle(e) {
+        e.preventDefault();
+        const kisi = new Kisi(this.ad.value, this.soyad.value, this.mail.value);
+        const sonuc = Util.bosAlanKontrolEt(kisi.ad, kisi.soyad, kisi.mail);
+
+        //Tüm aalanlar kontrol - dolu mu boş var mı
+        if (sonuc) {
+            //console.log("Başarılı");
+            this.kisiyiEkranaEkle(kisi);
+
+            //yeni kişiyi ekrana ekler - localstorage
+            this.depo.kisiEkle(kisi)
+        } else {
+            //console.log("Boş Alan Var");
+
+        }
     }
 }
 
 class Depo {
     constructor() {
-        this.tumKisiler = []; //Bu deponun geçtiği her yerde geçerli
+        this.tumKisiler = this.kisileriGetir(); //Bu deponun geçtiği her yerde geçerli
     }
     //Uygulama ilk açıldığında veriler getirilecektir.
     kisileriGetir() {
-        let tumKisilerLocal = [];
-        if(localStorage.getItem("tumKisiler")=== null){
+        let tumKisilerLocal;
+        if (localStorage.getItem("tumKisiler") === null) {
             tumKisilerLocal = [];
         } else {
             tumKisilerLocal = JSON.parse(localStorage.getItem("tumKisiler"));
         }
-        this.tumKisiler = tumKisilerLocal;
+        return tumKisilerLocal;
     }
 
-    kisiEkle(kisi){
-        const tumKisilerLocal = this.kisileriGetir(); 
+    kisiEkle(kisi) {
+        const tumKisilerLocal = this.kisileriGetir();
         tumKisilerLocal.push(kisi);
-        localStorage.setItem("tumKisiler"), JSON.stringify(tumKisilerLocal);
+        localStorage.setItem("tumKisiler" , JSON.stringify(tumKisilerLocal));
     }
 }
 
-const cekap = new Kisi("yakup", "sari", "cekapcheck@gmail.com");
-const ekran = new Ekran();
+document.addEventListener("DOMContentLoaded", function (e) {
+    const ekran = new Ekran();
+});
